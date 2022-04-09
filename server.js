b751c0867f62b79a5becb989f0d3ab2942210160
panjunjie <2535103724@qq.com>
2022-04-10 01:42:43 +0800

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require("http");
+let users = [
+    {id: 1, name: "xiaoming"},
+    {id: 2, name: "xiaohong"}
+];
+
+const server = http.createServer(function(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    if (req.url === "/api/users") {
+        res.end(JSON.stringify(users));
+    } else {
+        res.end("Not Found");
+    }
+});
+
+server.listen(8080, function() {
+    console.log("server is start, listen port 8080");
+});
